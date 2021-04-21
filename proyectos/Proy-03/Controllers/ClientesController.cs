27db650a7ff3631c0f9de9b1311ce060140680ed
gerardo <contacto@gerardobeltran.mx>
2021-04-21 11:50:49 -0600

    .

--- proyectos/Proy-03/Controllers/ClientesController.cs
+++ proyectos/Proy-03/Controllers/ClientesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
+using Proy_03.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Proy_03.Controllers
@@ -11,6 +13,24 @@ namespace Proy_03.Controllers
     [Route("api/[controller]")]
     public class ClientesController : Controller
     {
+
+        string arch = @"/Users/gerardo/JSON/clientes.json";
+        // string arch = @"c:\clientes.json";
+
+        List<Persona> Clientes;
+
+        public ClientesController()
+        {
+            // Abre el archivo json
+            StreamReader jsonStream = System.IO.File.OpenText(arch);
+            // Leer el archivo json
+            string json = jsonStream.ReadToEnd();
+            Clientes = JsonConvert.DeserializeObject<List<Persona>>(json);
+
+        }
+
+
+
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
