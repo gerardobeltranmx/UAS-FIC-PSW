e0c5c637b5beb9ebad8362349624c1966205ba30
gerardo <contacto@gerardobeltran.mx>
2021-04-22 11:46:30 -0600

    .

--- proyectos/Proy-03/Models/Producto.cs
+++ proyectos/Proy-03/Models/Producto.cs
@@ -3,6 +3,9 @@ namespace Proy_03.Models
 {
     public class Producto
     {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
         public Producto()
         {
         }
