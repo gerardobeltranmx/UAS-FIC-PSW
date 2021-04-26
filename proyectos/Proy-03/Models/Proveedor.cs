1563df4a0fe2d59a0c2da4fd1dd897d8f15eee74
gerardo <contacto@gerardobeltran.mx>
2021-04-26 11:44:48 -0600

    -

--- proyectos/Proy-03/Models/Proveedor.cs
+++ proyectos/Proy-03/Models/Proveedor.cs
@@ -3,10 +3,11 @@ namespace Proy_03.Models
 {
     public class Proveedor
     {
-        //id, nombre, domicilio, RFC, CABLE
+        //id, nombre, domicilio, RFC, CLABE
 
         public Proveedor()
         {
+
         }
     }
 }
