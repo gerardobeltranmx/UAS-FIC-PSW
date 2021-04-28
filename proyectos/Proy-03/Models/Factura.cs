a1385489546b70974838c962a32f236a17d37036
gerardo <contacto@gerardobeltran.mx>
2021-04-28 11:23:21 -0600

    -

---
+++ proyectos/Proy-03/Models/Factura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Proy_03.Models
+{
+    public class Factura
+    {
+        public int id { get; set; }
+        public int idCliente { get; set; }
+        public double SubTotal { get; set; }
+        public double IVA { get; set; }
+        public double Total { get; set; }
+
+        public Factura(int id, int idCliente, double SubTotal, double IVA, double Total)
+        {
+            this.id = id;
+            this.idCliente = idCliente;
+            this.SubTotal = SubTotal;
+            this.Total = Total;
+        }
+    }
+}
