72abc0326fca7be8a6c74b3889751874b932b371
gerardo <contacto@gerardobeltran.mx>
2021-05-06 11:51:03 -0600

    -

--- proyectos/Control-de-Errores/Program.cs
+++ proyectos/Control-de-Errores/Program.cs
@@ -16,11 +16,27 @@ namespace Control_de_Errores
 
                 M = N * 2;
 
+
+
                 Console.WriteLine("El doble de N es {0}", M);
+
+                M = M / N;
+
+                Console.WriteLine("M entre N es {0}", M);
+
             }
-            catch (OverflowException ex)
+            catch (OverflowException )
             {
                 Console.WriteLine("Error: Solo se aceptan valor de {0} a {1}", int.MinValue, int.MaxValue);
+            }
+            catch( FormatException )
+            {
+                Console.WriteLine("Solo se aceptan numeros!!!");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Se encontro que N es cero");
             }
                 
             catch(Exception ex)
