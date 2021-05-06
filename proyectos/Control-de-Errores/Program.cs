88e24357add40485f814e7bc306713a3ccedc767
gerardo <contacto@gerardobeltran.mx>
2021-05-06 11:39:04 -0600

    -

---
+++ proyectos/Control-de-Errores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Control_de_Errores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, M;
+
+            try
+            {
+                Console.Write("Escribe un numero: ");
+                N = int.Parse(Console.ReadLine());
+
+                M = N * 2;
+
+                Console.WriteLine("El doble de N es {0}", M);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: Solo se aceptan valor de {0} a {1}", int.MinValue, int.MaxValue);
+            }
+                
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+        }
+    }
+}
