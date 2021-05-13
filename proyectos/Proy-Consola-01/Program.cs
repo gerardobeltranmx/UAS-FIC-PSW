c2bcfeec385885eed7c75eeaf226c9d5932a352d
gerardo <contacto@gerardobeltran.mx>
2021-05-13 11:35:05 -0600

    -

---
+++ proyectos/Proy-Consola-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace Proy_Consola_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            WebClient cli = new WebClient();
+
+            cli.Headers.Add("Content-Type:application/json");
+            cli.Headers.Add("Accept:application/json");
+
+            var resultado = cli.DownloadString("https://localhost:8080/api/clientes/");
+
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
