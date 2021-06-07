301c94d8a7d73889d82bb5e9b9f55d54cf11ad3f
gerardo <contacto@gerardobeltran.mx>
2021-06-07 11:54:07 -0600

    -

---
+++ proyectos/Proy-Consola-02/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Proy_Consola_02.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public int existencia { get; set; }
+
+        public Producto(int id, string descripcion, decimal precio, int existencia)
+        {
+        }
+    }
+}
